eaf9c494aa8d1657ee76147a3c054fd8f2dc7253
nataliekuechler <nataliekuechler@outlook.com>
2022-10-21 10:30:45 -0500

    updated code

---
+++ DungeonLibrary/Monster.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Monster : Character
+    {
+        //FIELDS
+        //MinDamage as an int - can't be more than MaxDamage, or less than 0
+
+        //PROPERTIES
+        //MinDamage as an int - can't be more than MaxDamage, or less than 0
+        //MaxDamage as an int
+        //Description as a string
+
+        //CONSTRUCTORS
+
+
+
+        //METHODS
+        public Monster()
+        {
+
+        }
+        public Monster(string name, int hitChance, int block, int maxLife) : base(name, hitChance, block, maxLife)
+        {
+            //Remember to set MaxDamage first!!!!!! -- MinDamage will reference MaxDamage
+        }
+
+        public override string ToString()
+        {
+            return $"----- {Name} -----\n" +
+             $"Life: {Life} of {MaxLife}\n" +
+             //Put Damage here---MinDamage to MaxDamage
+             $"Hit Chance: {HitChance}%\n" +
+             $"Block: {Block}";
+            //Put Description here
+        }
+        public override int CalcDamage()
+        {
+            return base.CalcDamage();
+            //return a random number between Monster min and max damage
+
+        }
+
+    } //end class
+}//end namespace
---
+++ DungeonLibrary/Player.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    public class Player : Character
+    {
+
+        //FIELDS // No Fields
+
+        //PROPERTIES 
+        //Race CharacterRace
+        //EquippedWeapon Weapon
+        //CONSTRUCTORS
+        public Player(string name, int hitChance, int block, int maxLife) : base(name, hitChance, block, maxLife)
+        {
+            //handle unique assignment
+
+           
+            //Potential Expansion- not required
+
+            //switch (CharacterRace)
+            // {
+            // case Race.Elf
+                //HitChance += 5
+                //MaxHealth -= 5
+                //break;
+                //}
+        }
+
+        public Player()
+        {
+
+        }
+
+        //METHODS
+        public override int CalcDamage()
+        {
+            //return a random value between the Weapon's min and max damage
+            return 0;
+        }
+        public override int CalcHitChance()
+        {
+            return 0;
+            //HitChance + Weapon BonusHitChance
+        }
+        public override string ToString()
+        {
+            //CREATE a holding variable for the description
+            //THEN create a Switch on CharacterRace
+            //Description = "Whatever"
+            //case CharacterRace.Elf:    --"Describe an Elf"
+            // break;
+            return base.ToString(); //+ some unique description based on player race
+            //HINT- use a switch
+        }
+
+    }//end class
+}//end namespace
